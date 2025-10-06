d20fc4803ae57c79550472a30bb2b21cf0caa05c
Madan Kundu <madan@outrightsolutions.net>
2025-10-06 10:38:53 +0530

    update

--- app/layout.js
+++ app/layout.js
@@ -6,7 +6,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <head>
         <link
           rel="stylesheet"
