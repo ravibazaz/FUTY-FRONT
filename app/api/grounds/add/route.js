7b8a5ee021f96308844bc544d82718a42b47f96c
Madan Kundu <madan@outrightsolutions.net>
2025-11-11 15:19:39 +0530

    Update

--- app/api/grounds/add/route.js
+++ app/api/grounds/add/route.js
@@ -6,28 +6,28 @@ import { z } from "zod";
 import { v4 as uuidv4 } from "uuid";
 import path from "path";
 import { promises as fs } from "fs";
-export const GroundSchema = z.object({
-  name: z.string().nonempty("Ground Name is required").min(2, "Ground must be at least 2 character"),
-  add1: z.string().nonempty("Address 1 is required").min(2, "Address 1 must be at least 2 character"),
-  content: z.string().nonempty("Ground Facilities is required").min(2, "Ground Facilities must be at least 2 character"),
-  county: z.string().nonempty("County is required").min(2, "County must be at least 2 character"),
-  pin: z.string().nonempty("Post Code is required").min(2, "Post Code must be at least 2 character"),
-  isHomeGround: z.string().nonempty("Home Ground is required").min(2, "Home Ground must be at least 2 character"),
-  images: z
-    .union([
-      z.instanceof(File), //  single file
-      z.array(z.instanceof(File)).nonempty("At least one image is required"), //  multiple
-    ])
-    .refine(
-      (val) => {
-        const files = Array.isArray(val) ? val : [val];
-        return files.every((file) =>
-          ["image/jpeg", "image/png", "image/webp", "image/gif"].includes(file.type)
-        );
-      },
-      { message: "Only JPEG, PNG, GIF, or WebP files are allowed" }
-    ),
-});
+// export const GroundSchema = z.object({
+//   name: z.string().nonempty("Ground Name is required").min(2, "Ground must be at least 2 character"),
+//   add1: z.string().nonempty("Address 1 is required").min(2, "Address 1 must be at least 2 character"),
+//   content: z.string().nonempty("Ground Facilities is required").min(2, "Ground Facilities must be at least 2 character"),
+//   county: z.string().nonempty("County is required").min(2, "County must be at least 2 character"),
+//   pin: z.string().nonempty("Post Code is required").min(2, "Post Code must be at least 2 character"),
+//   isHomeGround: z.string().nonempty("Home Ground is required").min(2, "Home Ground must be at least 2 character"),
+//   images: z
+//     .union([
+//       z.instanceof(File), //  single file
+//       z.array(z.instanceof(File)).nonempty("At least one image is required"), //  multiple
+//     ])
+//     .refine(
+//       (val) => {
+//         const files = Array.isArray(val) ? val : [val];
+//         return files.every((file) =>
+//           ["image/jpeg", "image/png", "image/webp", "image/gif"].includes(file.type)
+//         );
+//       },
+//       { message: "Only JPEG, PNG, GIF, or WebP files are allowed" }
+//     ),
+// });
 
 export async function POST(req) {
 
@@ -57,47 +57,47 @@ export async function POST(req) {
     
     console.log(images);
     
-    const facilities = formData.getAll("facilities");
-    if (!Array.isArray(images)) images = [images]; //  handle single upload gracefully
+    // const facilities = formData.getAll("facilities");
+    // if (!Array.isArray(images)) images = [images]; //  handle single upload gracefully
 
-    //Validate with Zod
-    const result = GroundSchema.safeParse({ ...rawData, images });
-    // If validation fails, return an error response
-    if (!result.success) {
-      // Flatten errors to match your desired response structure
-      const errors = result.error.flatten().fieldErrors;
-      return NextResponse.json(
-        {
-          success: false,
-          message: Object.fromEntries(
-            Object.entries(errors).map(([key, value]) => [key, value[0]])
-          ),
-        },
-        { status: 200 }
-      );
-    }
-    const uploadDir = path.join(process.cwd(), "uploads/grounds");
-    await fs.mkdir(uploadDir, { recursive: true });
-    // Save uploaded images
-    const imagePaths = [];
-    for (const image of images) {
-      if (!image?.name) continue; // skip invalid entries
+    // //Validate with Zod
+    // const result = GroundSchema.safeParse({ ...rawData, images });
+    // // If validation fails, return an error response
+    // if (!result.success) {
+    //   // Flatten errors to match your desired response structure
+    //   const errors = result.error.flatten().fieldErrors;
+    //   return NextResponse.json(
+    //     {
+    //       success: false,
+    //       message: Object.fromEntries(
+    //         Object.entries(errors).map(([key, value]) => [key, value[0]])
+    //       ),
+    //     },
+    //     { status: 200 }
+    //   );
+    // }
+    // const uploadDir = path.join(process.cwd(), "uploads/grounds");
+    // await fs.mkdir(uploadDir, { recursive: true });
+    // // Save uploaded images
+    // const imagePaths = [];
+    // for (const image of images) {
+    //   if (!image?.name) continue; // skip invalid entries
 
-      const uniqueName = `${Date.now()}_${image.name}`;
-      const filePath = path.join(uploadDir, uniqueName);
-      const buffer = Buffer.from(await image.arrayBuffer());
-      await fs.writeFile(filePath, buffer);
+    //   const uniqueName = `${Date.now()}_${image.name}`;
+    //   const filePath = path.join(uploadDir, uniqueName);
+    //   const buffer = Buffer.from(await image.arrayBuffer());
+    //   await fs.writeFile(filePath, buffer);
 
-      imagePaths.push(`/uploads/grounds/${uniqueName}`);
-    }
+    //   imagePaths.push(`/uploads/grounds/${uniqueName}`);
+    // }
 
-    // Otherwise, it means the user is authenticated
-    await connectDB();
-    const newGround = await Grounds.create({
-      ...rawData,
-      images: imagePaths,
-      facilities: facilities
-    });
+    // // Otherwise, it means the user is authenticated
+    // await connectDB();
+    // const newGround = await Grounds.create({
+    //   ...rawData,
+    //   images: imagePaths,
+    //   facilities: facilities
+    // });
 
     return NextResponse.json({
       success: true,
