e0664ddeb88b658a8a350386e90998c044d5d55f
Madan Kundu <madan@outrightsolutions.net>
2025-12-01 17:12:04 +0530

    Update

---
+++ app/admin/tournaments/[id]/view/page.jsx
@@ -0,0 +1,297 @@
+import { connectDB } from "@/lib/db";
+import Image from "next/image";
+import Link from "next/link";
+import Teams from "@/lib/models/Teams";
+import Clubs from "@/lib/models/Clubs";
+import Leagues from "@/lib/models/Leagues";
+import Grounds from "@/lib/models/Grounds";
+import Tournaments from "@/lib/models/Tournaments";
+export default async function ViewTournamentPage({ params }) {
+    const id = (await params).id;
+    await connectDB();
+    const tournaments = await Tournaments.findById(id).populate("team_id", "name").populate("manager_id", "name").populate("league_id", "title").populate("ground", "name").populate("club", "name").lean();
+    console.log(tournaments);
+
+    return (
+        <>
+            <main className="main-body col-md-9 col-lg-9 col-xl-10">
+                <div className="body-top d-flex flex-wrap justify-content-between align-items-center gap-20 mb-10">
+                    <div className="top-left">
+                        <p className="top-breadcrumb mb-0">{'> Tournaments'}</p>
+                    </div>
+                    <div className="top-right d-flex justify-content-between align-items-center gap-10">
+                        {/* <a className="btn btn-common" href="clubs-new.php">New</a> */}
+                        <a href="#">
+                            <img src="/images/icon-setting.svg" alt="Settings" />
+                        </a>
+                    </div>
+                </div>
+                <div className="body-title-bar d-flex flex-wrap justify-content-between align-items-center gap-20 mb-20">
+                    <div className="body-title-bar-left d-flex flex-wrap align-items-center gap-20-70">
+                        <h1 className="page-title">{tournaments.name}</h1>
+                    </div>
+                </div>
+
+                <div className="body-main-cont">
+                    <div className="single-body-row row">
+                        <div className="single-body-left col-lg-12 col-xl-7">
+                            <div className="left-info-box">
+                                <div className="left-row row">
+                                    <div className="left-label-col col-md-5 col-lg-4 col-xl-4">
+                                        <div className="label-text">
+                                            <p className="mb-0">Date</p>
+                                        </div>
+                                    </div>
+                                    <div className="left-info-col col-md-7 col-lg-8 col-xl-8">
+                                        <div className="info-text">
+                                            <p className="mb-0">
+                                                <a className="text-primary text-decoration-none" href="#">{new Date(tournaments.date).toLocaleDateString()}</a>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="left-info-box">
+                                <div className="left-row row">
+                                    <div className="left-label-col col-md-5 col-lg-4 col-xl-4">
+                                        <div className="label-text">
+                                            <p className="mb-0">Time</p>
+                                        </div>
+                                    </div>
+                                    <div className="left-info-col col-md-7 col-lg-8 col-xl-8">
+                                        <div className="info-text">
+                                            <p className="mb-0">
+                                                <a className="text-primary text-decoration-none" href="#">{tournaments.time}</a>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="left-info-box">
+                                <div className="left-row row">
+                                    <div className="left-label-col col-md-5 col-lg-4 col-xl-4">
+                                        <div className="label-text">
+                                            <p className="mb-0">Description</p>
+                                        </div>
+                                    </div>
+                                    <div className="left-info-col col-md-7 col-lg-8 col-xl-8">
+                                        <div className="info-text">
+                                            <p className="mb-0">
+                                                <a className="text-primary text-decoration-none" href="#">{tournaments.description}</a>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="left-info-box">
+                                <div className="left-row row">
+                                    <div className="left-label-col col-md-5 col-lg-4 col-xl-4">
+                                        <div className="label-text">
+                                            <p className="mb-0">Venue</p>
+                                        </div>
+                                    </div>
+                                    <div className="left-info-col col-md-7 col-lg-8 col-xl-8">
+                                        <div className="info-text">
+                                            <p className="mb-0">
+                                                <a className="text-primary text-decoration-none" href="#">{tournaments.venue}</a>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="left-info-box">
+                                <div className="left-row row">
+                                    <div className="left-label-col col-md-5 col-lg-4 col-xl-4">
+                                        <div className="label-text">
+                                            <p className="mb-0">Total Amount</p>
+                                        </div>
+                                    </div>
+                                    <div className="left-info-col col-md-7 col-lg-8 col-xl-8">
+                                        <div className="info-text">
+                                            <p className="mb-0">
+                                                <a className="text-primary text-decoration-none" href="#">{tournaments.total_amount}</a>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="left-info-box">
+                                <div className="left-row row">
+                                    <div className="left-label-col col-md-5 col-lg-4 col-xl-4">
+                                        <div className="label-text">
+                                            <p className="mb-0">Email</p>
+                                        </div>
+                                    </div>
+                                    <div className="left-info-col col-md-7 col-lg-8 col-xl-8">
+                                        <div className="info-text">
+                                            <p className="mb-0">
+                                                <a className="text-primary text-decoration-none" href="#">{tournaments.email}</a>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="left-info-box">
+                                <div className="left-row row">
+                                    <div className="left-label-col col-md-5 col-lg-4 col-xl-4">
+                                        <div className="label-text">
+                                            <p className="mb-0">Contact</p>
+                                        </div>
+                                    </div>
+                                    <div className="left-info-col col-md-7 col-lg-8 col-xl-8">
+                                        <div className="info-text">
+                                            <p className="mb-0">
+                                                <a className="text-primary text-decoration-none" href="#">{tournaments.contact}</a>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="left-info-box">
+                                <div className="left-row row">
+                                    <div className="left-label-col col-md-5 col-lg-4 col-xl-4">
+                                        <div className="label-text">
+                                            <p className="mb-0">Notes</p>
+                                        </div>
+                                    </div>
+                                    <div className="left-info-col col-md-7 col-lg-8 col-xl-8">
+                                        <div className="info-text">
+                                            <p className="mb-0">
+                                                <a className="text-primary text-decoration-none" href="#">{tournaments.notes}</a>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="left-info-box">
+                                <div className="left-row row">
+                                    <div className="left-label-col col-md-5 col-lg-4 col-xl-4">
+                                        <div className="label-text">
+                                            <p className="mb-0">Team</p>
+                                        </div>
+                                    </div>
+                                    <div className="left-info-col col-md-7 col-lg-8 col-xl-8">
+                                        <div className="info-text">
+                                            <p className="mb-0">
+                                                <a className="text-primary text-decoration-none" href="#">{tournaments.team_id?.name}</a>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="left-info-box">
+                                <div className="left-row row">
+                                    <div className="left-label-col col-md-5 col-lg-4 col-xl-4">
+                                        <div className="label-text">
+                                            <p className="mb-0">Manager</p>
+                                        </div>
+                                    </div>
+                                    <div className="left-info-col col-md-7 col-lg-8 col-xl-8">
+                                        <div className="info-text">
+                                            <p className="mb-0">
+                                                <a className="text-primary text-decoration-none" href="#">{tournaments.manager_id?.name}</a>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="left-info-box">
+                                <div className="left-row row">
+                                    <div className="left-label-col col-md-5 col-lg-4 col-xl-4">
+                                        <div className="label-text">
+                                            <p className="mb-0">League</p>
+                                        </div>
+                                    </div>
+                                    <div className="left-info-col col-md-7 col-lg-8 col-xl-8">
+                                        <div className="info-text">
+                                            <p className="mb-0">
+                                                <a className="text-primary text-decoration-none" href="#">{tournaments.league_id?.title}</a>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="left-info-box">
+                                <div className="left-row row">
+                                    <div className="left-label-col col-md-5 col-lg-4 col-xl-4">
+                                        <div className="label-text">
+                                            <p className="mb-0">Club</p>
+                                        </div>
+                                    </div>
+                                    <div className="left-info-col col-md-7 col-lg-8 col-xl-8">
+                                        <div className="info-text">
+                                            <p className="mb-0">
+                                                <a className="text-primary text-decoration-none" href="#">{tournaments.club?.name}</a>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="left-info-box">
+                                <div className="left-row row">
+                                    <div className="left-label-col col-md-5 col-lg-4 col-xl-4">
+                                        <div className="label-text">
+                                            <p className="mb-0">Ground</p>
+                                        </div>
+                                    </div>
+                                    <div className="left-info-col col-md-7 col-lg-8 col-xl-8">
+                                        <div className="info-text">
+                                            <p className="mb-0">
+                                                <a className="text-primary text-decoration-none" href="#">{tournaments.ground?.name}</a>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="left-info-box">
+                                <div className="left-row row">
+                                    <div className="left-label-col col-md-5 col-lg-4 col-xl-4">
+                                        <div className="label-text mb-0">
+                                        </div>
+                                    </div>
+                                    <div className="left-info-col col-md-7 col-lg-8 col-xl-8">
+                                        <div className="info-text">
+                                            <p className="mb-0">
+                                                {/* <Link className="btn-common-text" href={`/admin/stores/${tournaments._id}/edit`} >Edit</Link> */}
+                                                <Link className="btn-common-text ps-3" href={`/admin/tournaments`} >Back</Link>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="single-body-right col-lg-12 col-xl-5">
+                            <div className="right-info-box">
+                                {/* <h2 className="info-box-title fs-14 fw-bold mb-30">Contact Details</h2> */}
+                                <div className="right-info mb-30">
+                                    {/* <Link href={`/admin/stores/${tournaments._id}/edit`}>
+                                        <Image
+                                            src={preview}
+                                            width={82}
+                                            height={82}
+                                            className={'profile-img mb-10'}
+                                            alt="Club Badge"
+                                        />
+                                    </Link>
+                                    <p className="mb-0">
+                                        <Link className="text-decoration-none fs-14 fw-bold text-primary underline-hover" href={`/admin/stores/${tournaments._id}/edit`}>Product Badge</Link>
+                                    </p> */}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+                </div>
+
+            </main>
+        </>
+
+    )
+
+}
